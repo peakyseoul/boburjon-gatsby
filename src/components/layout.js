573640f7012c05d0088064246ca3a988e53a67e3
peakyseoul <81403511+peakyseoul@users.noreply.github.com>
2022-04-22 13:19:33 +0900

    navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,17 +15,10 @@ const Layout = ({ location, title, children }) => {
             </Link>  
             </h1>
             <nav>
-                <ul >
-                    <li>
-                    <Link to = "/">Home</Link>
-                    </li>
-                    <li>
-                        <Link   to = "/about">About</Link>
-                    </li>
-                    <li>
-                        <Link   to = "/contact">Contact</Link>
-                    </li>
-                </ul>
+              <Link to = "/">Home</Link>
+              <Link   to = "/about">About</Link> 
+              <Link   to = "/contact">Contact</Link>
+                 
             </nav>
         </header>
         
